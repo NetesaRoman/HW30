2f4a9c6c0f55b55f963ab81063f42a54182d1d73
NetesaRoman <netesroman8@gmail.com>
2023-03-07 15:53:09 +0200

    l2 commit

--- src/main/java/com/example/hw30/controller/L2Controller.java
+++ src/main/java/com/example/hw30/controller/L2Controller.java
@@ -1,6 +1,6 @@
 package com.example.hw30.controller;
 
-import com.example.hw30.service.GaussianElimination;
+import com.example.hw30.service.exceptions.IterativeSolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class L2Controller {
 
-    private double[] results = {0d, 0d, 0d, 0d};
+
+
 
     @GetMapping("/l2")
     public String l2() {
@@ -32,55 +33,52 @@ public class L2Controller {
                          @RequestParam("s00") double s00, @RequestParam("s01") double s01, @RequestParam("s02") double s02, @RequestParam("s03") double s03){
 
 
-        double[][] matrix = new double[4][4];
-        setMatrix( a00,  a01,  a02,  a03,  a10, a11,  a12,  a13, a20,  a21,  a22,  a23, a30, a31,  a32,  a33, matrix);
-
-        double[] sums = new double[4];
-
-        setSolutions(s00, s01, s02,s03, sums);
+        double[][] matrix = new double[4][5];
+        setMatrix(s00, s01, s02, s03, a00,  a01,  a02,  a03,  a10, a11,  a12,  a13, a20,  a21,  a22,  a23, a30, a31,  a32,  a33, matrix);
 
 
 
-        model.addAttribute("result1", Math.round(results[0] * 100.0) / 100.0);
-        model.addAttribute("result2", Math.round(results[1] * 100.0) / 100.0);
-        model.addAttribute("result3", Math.round(results[2] * 100.0) / 100.0);
+
+
 
         printMatr(matrix);
+        IterativeSolver iterativeSolver = new IterativeSolver(matrix);
+        iterativeSolver.printInfo();
+
         return "l2";
     }
 
-    private static void setSolutions(double s00, double s01, double s02,  double s03, double[] sums) {
-        sums[0] = s00;
-        sums[1] = s01;
-        sums[2] = s02;
-        sums[3] = s03;
-    }
 
-    private static void setMatrix(double a00, double a01, double a02, double a03, double a10, double a11, double a12, double a13,double a20, double a21, double a22, double a23,double a30, double a31, double a32, double a33, double[][] matrix) {
+
+    private static void setMatrix(double s00, double s01, double s02,  double s03, double a00, double a01, double a02, double a03, double a10, double a11, double a12, double a13,double a20, double a21, double a22, double a23,double a30, double a31, double a32, double a33, double[][] matrix) {
         matrix[0][0] = a00;
         matrix[0][1] = a01;
         matrix[0][2] = a02;
         matrix[0][3] = a03;
+        matrix[0][4] = s00;
 
         matrix[1][0] = a10;
         matrix[1][1] = a11;
         matrix[1][2] = a12;
         matrix[1][3] = a13;
+        matrix[1][4] = s01;
 
         matrix[2][0] = a20;
         matrix[2][1] = a21;
         matrix[2][2] = a22;
         matrix[2][3] = a23;
+        matrix[2][4] = s02;
 
         matrix[3][0] = a30;
         matrix[3][1] = a31;
         matrix[3][2] = a32;
         matrix[3][3] = a33;
+        matrix[3][4] = s03;
     }
 
     public void printMatr(double[][] matrix){
         for(int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++){
+            for (int j = 0; j < 5; j++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println("\n");
---
+++ src/main/java/com/example/hw30/service/exceptions/IterativeSolver.java
@@ -0,0 +1,95 @@
+package com.example.hw30.service.exceptions;
+
+/*
+ *
+ * @author Roman Netesa
+ *
+ */
+public class IterativeSolver {
+    private double[][] A = {{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}}; // матрица коэффициентов
+    private double[] b = {3, 3, 3, 3}; // вектор правой части
+    private double[] x0 = {0, 0, 0, 0}; // начальное приближение
+    private double epsilon = 0.0001; // заданная точность
+    private int maxIterations = 1000; // максимальное количество итераций
+
+    public IterativeSolver(double[][] matrix){
+        for (int i = 0;i < 4 ;i++){
+            for (int j = 0;j < 4 ;j++){
+                A[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < 4; i++){
+            b[i] = matrix[i][4];
+        }
+    }
+
+    public void solve() {
+
+
+        // вычисление матрицы перехода
+        double[][] t = new double[4][4];
+        double[] d = new double[4];
+        for (int i = 0; i < 4; i++) {
+            d[i] = A[i][i];
+            for (int j = 0; j < 4; j++) {
+                t[i][j] = (i == j) ? 0 : -A[i][j] / d[i];
+            }
+        }
+
+        // вычисление вектора смещения
+        double[] c = new double[4];
+        for (int i = 0; i < 4; i++) {
+            c[i] = b[i] / d[i];
+        }
+
+        // итерационный процесс
+        double[] x = x0;
+        int iterations = 0;
+        while (iterations < maxIterations) {
+            double[] xNew = new double[4];
+            for (int i = 0; i < 4; i++) {
+                xNew[i] = 0;
+                for (int j = 0; j < 4; j++) {
+                    xNew[i] += t[i][j] * x[j];
+                }
+                xNew[i] += c[i];
+            }
+            if (converged(x, xNew, epsilon)) {
+                break;
+            }
+            x = xNew;
+            iterations++;
+        }
+
+        // вывод результата
+        System.out.println("Solution:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("x[" + i + "] = " + x[i]);
+        }
+    }
+
+    // проверка на достижение заданной точности
+    private  boolean converged(double[] x, double[] xNew, double epsilon) {
+        for (int i = 0; i < 4; i++) {
+            if (Math.abs(xNew[i] - x[i]) > epsilon) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void printInfo(){
+        for(int i = 0; i < 4 ; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(A[i][j] + " ");
+
+            }
+            System.out.println("\n");
+        }
+        System.out.println("-----------");
+        for(int i = 0; i < 4 ; i++){
+            System.out.print(b[i] + " ");
+        }
+    }
+}
